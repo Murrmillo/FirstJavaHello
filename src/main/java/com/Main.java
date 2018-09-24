704e7cc601a368d70f5498d102596fee1ed1b13a
Илья Остроух <ilya.ostrouh@yandex.ru>
2018-09-24 15:01:33 +0300

    Initial commit

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Alexandr Sergeevich!");
+
+        Second second = new Second();
+        second.Test();
+    }
+}
